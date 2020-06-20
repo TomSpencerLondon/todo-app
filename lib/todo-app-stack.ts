23349baab4e151703b89cdef106825c82a10f517
Tom <thomas.spencer@codurance.com>
2020-06-20 16:54:29 +0100

    Add isProduction env

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -9,7 +9,10 @@ export class TodoAppStack extends cdk.Stack {
     const helloLambda = new lambda.Function(this, 'HelloLambda', {
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
-      runtime: lambda.Runtime.NODEJS_12_X
+      runtime: lambda.Runtime.NODEJS_12_X,
+      memorySize: 256,
+      timeout: cdk.Duration.seconds(10),
+      environment: {isProduction: 'absolutely not'}
     });
 
     new apiGateway.LambdaRestApi(this, 'Endpoint', {
