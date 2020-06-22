0ee812ffbfc1692fb9cf82c62467df9d8512883f
Tom <thomas.spencer@codurance.com>
2020-06-22 16:15:04 +0100

    Fail: can't update s3 bucket on the stack

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apiGateway from '@aws-cdk/aws-apigateway';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     const helloLambda = new lambda.Function(this, 'HelloLambda', {
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
@@ -18,5 +19,20 @@ export class TodoAppStack extends cdk.Stack {
     new apiGateway.LambdaRestApi(this, 'Endpoint', {
       handler: helloLambda
     })
+
+    const samplebucket = new s3.Bucket(this, 'samplebucket', {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html'
+    });
+
+    const deployment = new s3Deployment.BucketDeployment(
+      this,
+      'deployStaticWebsite',
+      {
+        sources: [s3Deployment.Source.asset('../website')],
+        destinationBucket: samplebucket
+      }
+    );
   }
 }
