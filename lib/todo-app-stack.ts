d4688e990247fd971a83787be257dff71a4c0157
Tom <thomas.spencer@codurance.com>
2020-06-23 19:45:53 +0100

    Successful AWS deploy

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apiGateway from '@aws-cdk/aws-apigateway';
 import * as s3 from '@aws-cdk/aws-s3';
-import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -18,21 +17,8 @@ export class TodoAppStack extends cdk.Stack {
 
     new apiGateway.LambdaRestApi(this, 'Endpoint', {
       handler: helloLambda
-    })
-
-    const samplebucket = new s3.Bucket(this, 'samplebucket', {
-      publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      websiteIndexDocument: 'index.html'
     });
 
-    const deployment = new s3Deployment.BucketDeployment(
-      this,
-      'deployStaticWebsite',
-      {
-        sources: [s3Deployment.Source.asset('../website')],
-        destinationBucket: samplebucket
-      }
-    );
+    const logoBucket = new s3.Bucket(this, "logoBucket", {});
   }
 }
